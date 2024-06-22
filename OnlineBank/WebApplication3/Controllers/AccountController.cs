8969d46c287793f74edd8964ecd24edddcb4e695
Padilla, Patrick M. <161110953+PADILLA-PATRICK-BSIT32E3@users.noreply.github.com>
2024-06-22 15:54:09 +0800

    Update controller

--- OnlineBank/WebApplication3/Controllers/AccountController.cs
+++ OnlineBank/WebApplication3/Controllers/AccountController.cs
@@ -23,9 +23,9 @@ namespace WebApplication3.Controllers
         private readonly string twilioPhoneNumber = "+16465808947"; // Twilio phone number
 
 
-        private readonly string _infobipApiKey = "212952af9d98fc971b0aa3735e4d5684-9d867386-32dd-4a29-81f7-1bc5f1dbb225";
+        private readonly string _infobipApiKey = "feb1f237f083f04968dd83bd9a0a44b9-c1360cb7-e98b-4a0c-8e25-2af7bc57a041";
         private readonly string _infobipBaseUrl = "https://api.infobip.com/sms/2/text/single";
-        private readonly string _infobipPhoneNumber = "+639203614524";
+        private readonly string _infobipPhoneNumber = "+639060525801";
 
         [HttpPost]
         public async Task<IActionResult> SendOtp(string phoneNumber)
